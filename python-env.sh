1d6c0e3ce6e6a72f8e54f8d428773fff1bd58137
cassiobotaro <cassiobotaro@gmail.com>
2016-02-28 22:45:59 -0300

    Add docker-compose on toolbelt

--- python-env.sh
+++ python-env.sh
@@ -1,4 +1,4 @@
 sudo apt-get install -y python-setuptools python-dev python3-dev
 sudo easy_install pip
-sudo -H pip install -U  virtualenvwrapper jupyter flake8
+sudo -H pip install -U  virtualenvwrapper jupyter flake8 docker-compose
 echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
