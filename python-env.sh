4e1049adfccda89735a9e01d84b8246ea2984d18
cassiobotaro <cassiobotaro@gmail.com>
2016-03-25 18:32:07 -0300

    Added ipdb to python envinroment

--- python-env.sh
+++ python-env.sh
@@ -1,4 +1,4 @@
 sudo apt-get install -y python-setuptools python-dev python3-dev
 sudo easy_install pip
-sudo -H pip install -U  virtualenvwrapper jupyter flake8 docker-compose
+sudo -H pip install -U  virtualenvwrapper jupyter flake8 docker-compose ipdb
 echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
