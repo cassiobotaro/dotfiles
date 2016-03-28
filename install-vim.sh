0747650503dda8924af03668c2f72059537e12d0
cassiobotaro <cassiobotaro@gmail.com>
2016-03-28 08:30:24 -0300

    fix vim installation with vim-plug

--- install-vim.sh
+++ install-vim.sh
@@ -1,3 +1,6 @@
-sudo apt-get install -y vim vim-gtk git exuberant-ctags ncurses-term silversearcher-ag
-sudo -H pip install flake8 autopep8 isort
+sudo apt-get install -y vim vim-gtk git exuberant-ctags ncurses-term silversearcher-ag curl
+sudo -H pip install -U flake8 autopep8 isort
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/.vimrc
+vim +PlugInstall
