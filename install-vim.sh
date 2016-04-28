f1a34ea5dfcfa21d6132a5f1a4ab819c76325a09
Cássio Botaro <cassiobotaro@gmail.com>
2016-04-27 23:18:37 -0300

    Update install-vim.sh

--- install-vim.sh
+++ install-vim.sh
@@ -2,7 +2,8 @@ sudo apt-get install -y vim vim-gtk git exuberant-ctags ncurses-term silversearc
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
 sed -i -e 's/FontName=.*/FontName=Droid Sans Mono for Powerline 9/g' ~/.config/xfce4/terminal/terminalrc
-sudo -H pip install -U flake8 autopep8 isort
+sudo -H pip install -U flake8
+sudo -H pip3 install -U autopep8 isort
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cd -
