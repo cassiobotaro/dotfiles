4061d9bbc97c239fb45994dc0a70e5dc46d68c61
cassiobotaro <cassiobotaro@gmail.com>
2016-05-21 17:59:47 -0300

    Added monokai theme

--- install_custom_zsh.sh
+++ install_custom_zsh.sh
@@ -1,6 +1,7 @@
 # install oh-my-zsh
 sudo apt install -y zsh git python-setuptools autojump tree golang ttf-ancient-fonts python-pip
 cp .zshrc ~/.zshrc
+sudo cp monokai-dark.theme /usr/share/xfce4/terminal/colorschemes/
 sudo  -H pip install -U virtualenvwrapper
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s $(which zsh)
