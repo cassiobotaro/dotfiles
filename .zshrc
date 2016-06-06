98c3d6f1582275b3a61e24561756aaaeb8d77e2a
cassiobotaro <cassiobotaro@gmail.com>
2016-06-05 21:48:20 -0300

    pytest included in vim

--- .zshrc
+++ .zshrc
@@ -1,5 +1,4 @@
 # Path to your oh-my-zsh installation.
-export TERM="xterm-256color"
 export ZSH=$HOME/.oh-my-zsh
 
 # Set name of the theme to load.
--- install_custom_zsh.sh
+++ install_custom_zsh.sh
@@ -1,7 +1,6 @@
 # install oh-my-zsh
 sudo apt install -y zsh git python-setuptools autojump tree golang ttf-ancient-fonts python-pip
 cp .zshrc ~/.zshrc
-sudo cp monokai-dark.theme /usr/share/xfce4/terminal/colorschemes/
 sudo  -H pip install -U virtualenvwrapper
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s $(which zsh)
