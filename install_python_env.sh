9e95d23b86c882ec981714a3751ab1b78902007d
cassiobotaro <cassiobotaro@gmail.com>
2016-07-16 01:12:43 -0300

    remove ipython config

--- install_python_env.sh
+++ install_python_env.sh
@@ -1,5 +1,3 @@
 sudo apt install -y python-dev python3-dev python3-pip python-pip
 sudo -H pip install -U  virtualenvwrapper jupyter flake8 docker-compose ipdb
-mkdir -p ~/.ipython/profile_default/
-cp ipython_config.py ~/.ipython/profile_default/
 echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
