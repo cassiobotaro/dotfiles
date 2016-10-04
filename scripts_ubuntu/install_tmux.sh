0d79e4c55b290be3c4218a2cd2a1dbb56b6e7daa
cassiobotaro <cassiobotaro@gmail.com>
2016-10-04 00:20:55 -0300

    new configs for tmux

--- scripts_ubuntu/install_tmux.sh
+++ scripts_ubuntu/install_tmux.sh
@@ -1,6 +1,4 @@
 sudo apt install tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ../.tmux.conf ~/
 tmux source ~/.tmux.conf
-echo "Now type \"tmux\" and install plugins."
-echo "Press prefix + I (capital I, as in Install) to fetch the plugins."
+echo "Tmux is ok! "
