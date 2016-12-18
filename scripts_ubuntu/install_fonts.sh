1822bfe493037d7c07a315ab2086b66ee81ad2c9
cassiobotaro <cassiobotaro@gmail.com>
2016-12-17 22:22:18 -0200

    some changes on install_fonts

--- scripts_ubuntu/install_fonts.sh
+++ scripts_ubuntu/install_fonts.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+echo "Cloning fonts repository..."
 git clone https://github.com/powerline/fonts.git
+echo "Installing fonts..."
 ./fonts/install.sh
 sudo rm -r fonts/
 sudo apt install -y curl
+echo "Install Nerd Font"
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
+fc-cache -v
 echo "Now go to the terminal preferences and change your font."
