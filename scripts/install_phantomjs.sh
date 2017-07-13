475aa48d9571d087aeb698f9a032f27afd311dfd
cassiobotaro <cassiobotaro@gmail.com>
2017-07-13 01:13:28 -0300

    remove unecessary compacted file

--- scripts/install_phantomjs.sh
+++ scripts/install_phantomjs.sh
@@ -6,3 +6,4 @@ wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
 sudo tar xvjf $PHANTOM_JS.tar.bz2
 sudo mv $PHANTOM_JS /usr/local/share
 sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
+rm -R phantomjs-2.1.1-linux-x86_64.tar.bz2
