0c6a2a280519de33394a957c50f1abe82a3fd09b
cassiobotaro <cassiobotaro@gmail.com>
2017-07-28 23:51:32 -0300

    update 28-07-2017

---
+++ arch_scripts/install.sh
@@ -0,0 +1,25 @@
+yaourt -S chromedriver google-chrome slack-desktop git-core ctags ncurses curl spotify tmux neovim python-neovim python-pip zsh xclip phantomjs git autojump tree oh-my-zsh-git docker-compose docker geckodriver go tlp tlp-rdw
+chsh -s $(which zsh)
+sudo tlp start
+systemctl start docker
+systemctl enable docker
+sudo usermod -aG docker $USER
+su - $USER
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Knack Regular Nerd Font Complete Mono.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Regular/complete/Knack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+cd ~
+mkdir -p go
+mkdir -p go/{src,bin}
+go get -u github.com/golang/dep/cmd/dep
+go get -u github.com/derekparker/delve/cmd/dlv
+go get -u github.com/kardianos/govendor
+go get -u golang.org/x/tools/cmd/present
+go get -u github.com/alecthomas/gometalinter
+gometalinter -i
+sudo -H pip install --upgrade  virtualenvwrapper flake8 jedi
+mkdir -p ~/.config/nvim
+cp ../.zshrc ~/
+cp ../.tmux.conf ~/
+cp ../init.vim ~/.config/nvim
+cp ../local_init.vim ~/.config/nvim
+cp ../local_bundles.vim ~/.config/nvim
