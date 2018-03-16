166b3a629584fc9034d6a470c4b18b2fea5aa972
cassiobotaro <cassiobotaro@gmail.com>
2018-03-16 00:42:07 -0300

    :octocat: update 16/03/2018

--- .zshrc
+++ .zshrc
@@ -9,33 +9,25 @@ ZSH_THEME="steeef"
 
 DISABLE_UNTRACKED_FILES_DIRTY="true"
 
-plugins=(git extract virtualenvwrapper autojump  docker go tmux sudo docker-compose archlinux pyenv)
-
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+plugins=(
+    git
+    extract
+    virtualenvwrapper
+    autojump
+    docker
+    go
+    tmux
+    sudo
+    docker-compose
+    archlinux
+)
 
 source $ZSH/oh-my-zsh.sh
-export EDITOR='vim'
+export EDITOR='nvim'
 
-alias zshconfig="vim ~/.zshrc"
-alias ohmyzsh="vim ~/.oh-my-zsh"
+alias zshconfig="nvim ~/.zshrc"
+alias nvimconfig="nvim ~/.config/nvim/local_init.vim"
 alias tree="tree -C"
-
-# python aliases
-alias rmpyc='find . -name "__pycache__" -delete -or -iname "*.pyc" -delete'
-alias venv='python3 -m venv'
-alias serve='python3 -m http.server'
-alias pydoc='python3 -m pydoc'
-alias pytime='python3 -m timeit'
-alias pyprof='python3 -m profile'
-alias jcat='python3 -m json.tool'
-alias cal='python3 -m calendar'
-alias py2path='python2 -m site'
-alias py3path='python3 -m site'
-alias bytecode='python3 -m dis'
-alias web='python3 -m webbrowser'
-alias inspect='python3 -m inspect'
-alias tokenize='python3 -m tokenize'
-alias zipy='python3 -m zipfile'
 
 # go
 export GOROOT=/usr/lib/go
@@ -53,4 +45,3 @@ function ctop() {
 
 export NUVEO_DEBUG=true
 alias vim=nvim
-source $HOME/.cargo/env
--- install.sh
+++ install.sh
@@ -1,25 +1,21 @@
-yaourt -S chromedriver google-chrome slack-desktop git-core ctags ncurses curl spotify tmux neovim python-neovim python-pip zsh xclip phantomjs git autojump tree oh-my-zsh-git docker-compose docker geckodriver go tlp tlp-rdw ttf-dejavu vim gvim powerline-fonts ttf-ancient-fonts the_silver_searcher xfce4-clipman-plugin tp_smapi acpi_call gimp xfce4-whiskermenu-plugin xdg-utils xf86-video-intel
+yaourt -S slack-desktop git ctags ncurses curl spotify tmux neovim python-neovim python-pip zsh xclip autojump tree oh-my-zsh-git docker-compose docker go powerline-fonts the_silver_searcher xfce4-clipman-plugin gimp xfce4-whiskermenu-plugin ttf-nerd-fonts-knack-complete-git flake8 python-jedi jupyter autopep8 python-isort
+cp /usr/share/oh-my-zsh/zshrc ~/.zshrc
 chsh -s $(which zsh)
-sudo tlp start
-systemctl enable tlp.service
-systemctl enable tlp-sleep.service
-systemctl mask systemd-rfkill.service
 systemctl start docker
-systemctl enable docker
+sudo systemctl enable docker
 sudo usermod -aG docker $USER
 su - $USER
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Knack Regular Nerd Font Complete Mono.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Regular/complete/Knack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
 cd ~
-mkdir -p go
-mkdir -p go/{src,bin}
+export GOROOT=/usr/lib/go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 go get -u github.com/golang/dep/cmd/dep
 go get -u github.com/derekparker/delve/cmd/dlv
 go get -u github.com/kardianos/govendor
 go get -u golang.org/x/tools/cmd/present
 go get -u github.com/alecthomas/gometalinter
 gometalinter -i
-sudo pip install --upgrade virtualenvwrapper flake8-bugbear jedi ipython  bandit pylint pydocstyle pipenv radon autopep8 isort
+sudo pip install --upgrade pipenv radon virtualenvwrapper
 mkdir -p ~/.config/nvim
 cp .zshrc ~/
 cp .gitconfig ~/
@@ -27,6 +23,3 @@ cp .tmux.conf ~/
 cp init.vim ~/.config/nvim
 cp local_init.vim ~/.config/nvim
 cp local_bundles.vim ~/.config/nvim
-cp .vimrc ~/
-cp .vimrc.local ~/
-cp .vimrc.local.bundles ~/
