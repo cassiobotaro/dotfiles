81dafb7ba7596a86e8825b528358752383bcce53
cassiobotaro <cassiobotaro@gmail.com>
2019-01-31 01:15:14 -0200

    minor fix

--- ubuntu/install_chromedriver.sh
+++ ubuntu/install_chromedriver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install -y wget
+sudo apt install -y wget curl
 CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
 wget https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
 python3 -m zipfile -e chromedriver_linux64.zip .
