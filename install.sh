680b0ffd756c9cda8da4e56087123e8be04b3a02
Cássio Botaro <cassiobotaro@gmail.com>
2019-04-08 16:24:27 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-yay -S google-chrome slack-desktop git curl spotify tmux neovim python-pip zsh xclip autojump tree oh-my-zsh-git base-devel openssl zlib docker-compose docker pigz the_silver_searcher xfce4-clipman-plugin gimp xfce4-whiskermenu-plugin ttf-nerd-fonts-hack-complete-git ttf-vlgothic ttf-ancient-fonts ctags ncurses redshift pyenv  acpi_call lsb-release smartmontools tp_smapi ethtool
+yay -S google-chrome slack-desktop git curl go spotify tmux neovim python-pip zsh xclip autojump tree oh-my-zsh-git base-devel openssl zlib docker-compose docker pigz the_silver_searcher xfce4-clipman-plugin gimp xfce4-whiskermenu-plugin ttf-nerd-fonts-hack-complete-git ttf-vlgothic ttf-ancient-fonts ctags ncurses redshift pyenv  acpi_call lsb-release smartmontools tp_smapi ethtool
 pip install --user neovim jedi isort flake8 black
 cp .zshrc ~/
 cp .tmux.conf ~/
