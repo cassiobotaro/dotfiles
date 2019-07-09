47ffc29b1922aa71a32bd5f5f5045d473b5dacec
Cássio Botaro <cassiobotaro@gmail.com>
2019-07-09 02:35:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
-yay -S google-chrome slack-desktop git curl go spotify tmux neovim python-pip zsh xclip autojump tree oh-my-zsh-git base-devel openssl zlib docker-compose docker pigz the_silver_searcher xfce4-clipman-plugin gimp xfce4-whiskermenu-plugin ttf-nerd-fonts-hack-complete-git ttf-vlgothic ttf-ancient-fonts ctags ncurses redshift pyenv  acpi_call lsb-release smartmontools tp_smapi ethtool
-pip install --user neovim jedi isort flake8 black cookiecutter
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd - && sudo rm -R yay
+yay -S google-chrome slack-desktop git curl go spotify tmux neovim zsh xclip autojump tree oh-my-zsh-git base-devel openssl zlib xz docker the_silver_searcher gimp ttf-nerd-fonts-hack-complete-git ctags ncurses redshift pyenv tlp
+sudo python -m ensurepip
+python -m pip install --user neovim jedi isort flake8 black cookiecutter docker-compose
 cp .zshrc ~/
 cp .tmux.conf ~/
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim
 cp .gitconfig ~/
 chsh -s $(which zsh)
-sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
 sudo chown -R "$USER:$USER" "$ZSH_CACHE_DIR"
 su - $USER
+sudo tlp start
