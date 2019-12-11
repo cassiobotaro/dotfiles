5d5390f6e64faad75754cab9acc82ea51f7bc240
Cássio Botaro <cassiobotaro@gmail.com>
2019-12-11 08:21:04 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,29 @@
+# package manager
 sudo pacman -S yay
-yay -S google-chrome slack-desktop git curl go spotify tmux neovim zsh xclip autojump tree oh-my-zsh-git base-devel openssl zlib xz docker the_silver_searcher gimp ttf-nerd-fonts-hack-complete-git ctags ncurses redshift pyenv tlp
-sudo python -m ensurepip
-python -m pip install --user neovim jedi isort flake8 black cookiecutter docker-compose
+# missing apps
+yay -S base-devel
+yay -S google-chrome spotify gimp redshift tlp vlc
+mkdir ~/.cache/vlc
+sudo chown -R "$USER:$USER" "$ZSH_CACHE_DIR"
+sudo tlp start
+# terminal stuffs
+yay -S xsel autojump tree the_silver_searcher ttf-nerd-fonts-hack-complete-git ctags ncurses
+yay -S  tmux neovim zsh oh-my-zsh-git
+# set preferences
 cp .zshrc ~/
 cp .tmux.conf ~/
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim
 cp .gitconfig ~/
 chsh -s $(which zsh)
+# python environment
+yay -S openssl zlib xz
+yay -S pyenv
+sudo python -m ensurepip
+python -m pip install -U --user neovim jedi isort flake8 black cookiecutter docker-compose pip
+# other dev stuffs
+yay -S docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
-mkdir ~/.cache/vlc
-sudo chown -R "$USER:$USER" "$ZSH_CACHE_DIR"
+# END
 su - $USER
-sudo tlp start
