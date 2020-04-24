f0d2ff354ff5b0aeaad9277d09fa0c9d9df30cc5
Cássio Botaro <cassiobotaro@gmail.com>
2020-04-24 12:13:58 -0300

    Delete install_google_chrome.sh

--- ubuntu/install_google_chrome.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
