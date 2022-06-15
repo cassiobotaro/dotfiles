60ca7d9fb535b41856a42e7e7c19f4bde043ec8d
Cássio Botaro <cassiobotaro@gmail.com>
2022-06-15 15:45:14 -0300

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -11,7 +11,9 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 # spotify
-sudo snap install spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
 # others
 sudo apt install build-essential wget gimp redshift tlp
 sudo tlp start
@@ -22,8 +24,6 @@ ln -s $(which fdfind) ~/.local/bin/fd
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# nvim
-sudo snap install nvim --classic
 # github cli
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
@@ -32,9 +32,11 @@ sudo apt install gh
 mkdir $ZSH_CUSTOM/plugins/gh
 gh completion --shell zsh > $ZSH_CUSTOM/plugins/gh/_gh
 # bat
-wget https://github.com/sharkdp/bat/releases/download/v0.19.0/bat_0.19.0_amd64.deb
-sudo dpkg -i bat_0.19.0_amd64.deb
-rm bat_0.19.0_amd64.deb
+wget https://github.com/sharkdp/bat/releases/download/v0.21.0/bat_0.21.0_amd64.deb
+sudo dpkg -i bat_0.21.0_amd64.deb
+rm bat_0.21.0_amd64.deb
+# asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
 # set preferences
 cp .zshrc ~/
 cp .tmux.conf ~/
@@ -42,6 +44,7 @@ mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim
 cp .gitconfig ~/
 cp terminalrc ~/.config/xfce4/terminal/
+# nvim
 
 # python environment
 sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
